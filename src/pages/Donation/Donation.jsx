0af4696c2979349054c98129addbf1292f13454c
sumitrapaul <sumitra.cse.just2020@gmail.com>
2023-09-26 01:03:03 +0600

    add delete donation button in donation page

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import DataCard from "../CardDetails/DataCard";
+// import DataCard from "../CardDetails/DataCard";
 import DonationCard from "./DonationCard";
 
 const Donation = () => {
     const [donations,setDonations]=useState([])
-    const [noFound,setNoFound]=useState("")
+    const [noFound,setNoFound]=useState(false)
+
+    const [isShow,setIsShow]=useState(false)
     useEffect(()=>{
         const donateCards=JSON.parse(localStorage.getItem('donation'))
        if(donateCards){
@@ -15,15 +17,23 @@ const Donation = () => {
        }
     },[])
     console.log(donations)
+
+    const handleDeteleDonation=()=>{
+        localStorage.clear()
+        setDonations([])
+        setNoFound('Not found!!')
+    }
     return (
         <div>
            {
             noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> : <div>
-            <div className="grid grid-cols-2">
+                {donations.length > 0 && <button onClick={handleDeteleDonation} className="px-5 rounded-lg m-4 py-3 bg-green-700 text-white block mx-auto">Delete Donation</button>}
+            <div className="grid grid-cols-2 gap-5">
                 {
                     donations.map(card => <DonationCard key={card.id} card={card}></DonationCard>)
                 }
             </div>
+            <button onClick={() => setIsShow(!isShow)} className="px-5 rounded-lg m-4 py-3 bg-green-700 text-white block mx-auto">See All</button>
             </div>
 
            }
